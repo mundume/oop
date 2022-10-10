1de1520d05998ff37eec833b9bf8ba3c01f4375a
nzai <nzaih18@gmail.com>
2022-10-10 08:09:48 +0300

    oopjs

---
+++ app.js
@@ -0,0 +1,11 @@
+class Person {
+  constructor(name, location){
+   this.firstname = name
+   this.location = location
+  }
+}
+
+const nzai = new Person('nZAI', 'mariakani')
+const kilonzo = new Person(22, 23)
+console.log(kilonzo)
+console.log(nzai)
