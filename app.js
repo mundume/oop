eb2df19e60353bfe3745522d186eb1cb8403f340
nzai <nzaih18@gmail.com>
2022-10-10 09:32:31 +0300

    gangsteritus

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-class Person {
-    constructor(name, location, dob){
-     this.firstname = name
-     this.location = location
-     this.birthday = new Date(dob)
-    }
-    greeting(){
-      return `Hello ${this.firstname}`
-    }
-    getAge (){
-      const diff = Date.now() - this.birthday.getTime()
-      const ageDate = new Date(diff)
-      return Math.abs(ageDate.getUTCFullYear()-1970)
-    }
-    isMarried(name){
-     this.firstname = name
-    }
-    static add(x,y){
-      return x+y
-    }
+// class Person {
+//     constructor(name, location, dob){
+//      this.firstname = name
+//      this.location = location
+//      this.birthday = new Date(dob)
+//     }
+//     greeting(){
+//       return `Hello ${this.firstname}`
+//     }
+//     getAge (){
+//       const diff = Date.now() - this.birthday.getTime()
+//       const ageDate = new Date(diff)
+//       return Math.abs(ageDate.getUTCFullYear()-1970)
+//     }
+//     isMarried(name){
+//      this.firstname = name
+//     }
+//     static add(x,y){
+//       return x+y
+//     }
   
-  }
+//   }
   
-  const nzai = new Person('nZAI', 'mariakani', '07-13-1999')
-  const kilonzo = new Person(22, 23)
-  console.log(kilonzo.greeting())
-  console.log(nzai.getAge())
-  nzai.isMarried('Oriti')
-  console.log(nzai)
-  console.log(Person.add(2,4))
+//   const nzai = new Person('nZAI', 'mariakani', '07-13-1999')
+//   const kilonzo = new Person(22, 23)
+//   console.log(kilonzo.greeting())
+//   console.log(nzai.getAge())
+//   nzai.isMarried('Oriti')
+//   console.log(nzai)
+//   console.log(Person.add(2,4))
